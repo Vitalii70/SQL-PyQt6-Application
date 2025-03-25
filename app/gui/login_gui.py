8b6fa237f9f43dbee545c9f2b5069d99517ee3bf
Vitalii70 <nispolv@gmail.com>
2025-03-25 09:22:02 +0100

    Update syntax
    Syntax for login_gui.py

--- app/gui/login_gui.py
+++ app/gui/login_gui.py
@@ -7,9 +7,9 @@ This module provides the logic for user authentication, including input validati
 # PyQt6
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, \
-    QPushButton, QLabel, QLineEdit, QMessageBox
+    QPushButton, QLabel, QLineEdit
 
-# Extra library
+# Extra library (check if data right for database, QQS and error_message)
 from app.database_logic.database import control_data
 from ..config import QQS_LOGIN_GUI, show_error_message
 
@@ -41,16 +41,14 @@ class LoginScreen(QWidget):
         # Text Name of program
         label_main_text = QLabel("SQL & PyQt")
         label_main_text.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        label_main_text.setObjectName("mainTextLabel") # QSS
-        self.text_layout.addWidget(label_main_text,
-                                   alignment=Qt.AlignmentFlag.AlignCenter)
+        label_main_text.setObjectName("mainTextLabel")  # QSS
+        self.text_layout.addWidget(label_main_text, alignment=Qt.AlignmentFlag.AlignCenter)
 
         # lineedit for name
         self.name_lineedit = QLineEdit()
         self.name_lineedit.setPlaceholderText("Username")
         self.name_lineedit.setFixedSize(250, 40)
-        self.button_1_layout.addWidget(self.name_lineedit,
-                                       alignment=Qt.AlignmentFlag.AlignCenter)
+        self.button_1_layout.addWidget(self.name_lineedit, alignment=Qt.AlignmentFlag.AlignCenter)
 
         # lineedit for password
         self.password_lineedit = QLineEdit()
@@ -58,15 +56,14 @@ class LoginScreen(QWidget):
         # to hide the password when entering
         self.password_lineedit.setEchoMode(QLineEdit.EchoMode.Password)
         self.password_lineedit.setFixedSize(250, 40)
-        self.button_1_layout.addWidget(self.password_lineedit,
-                                       alignment=Qt.AlignmentFlag.AlignCenter)
+        self.button_1_layout.addWidget(self.password_lineedit, alignment=Qt.AlignmentFlag.AlignCenter)
 
         # for better arrangement
         self.button_1_layout.setSpacing(0)
         self.button_1_layout.setContentsMargins(0, 0, 0, 0)
 
         self.button_3 = QPushButton("Continue >")
-        self.button_3.setStyleSheet("button_continue") # QSS
+        self.button_3.setStyleSheet("button_continue")  # QSS
         self.button_3.setAutoDefault(True)
         self.button_3.setDefault(True)
         self.button_3.setFixedSize(150, 50)
@@ -75,13 +72,8 @@ class LoginScreen(QWidget):
 
         self.button_dont_acc = QPushButton("Create an account")
         self.button_dont_acc.setMinimumSize(175, 75)
-        self.button_dont_acc.clicked.connect(
-            lambda: self.stacked_windows.setCurrentIndex(1)
-        )
-        self.button_dont_acc_layout.addWidget(
-            self.button_dont_acc,
-            alignment=Qt.AlignmentFlag.AlignRight
-        )
+        self.button_dont_acc.clicked.connect(lambda: self.stacked_windows.setCurrentIndex(1))
+        self.button_dont_acc_layout.addWidget(self.button_dont_acc, alignment=Qt.AlignmentFlag.AlignRight)
 
         # Putting it all together
         self.main_screen_layout.addLayout(self.text_layout)
